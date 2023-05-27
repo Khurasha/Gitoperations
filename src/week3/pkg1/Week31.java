eb57ec21796644336c0c4a84d92f8e2db4858b84
khurasha@sheridancollege.ca <shabd@192.168.5.16>
2023-05-26 22:29:06 -0400

    Week 3

--- src/week3/pkg1/Week31.java
+++ src/week3/pkg1/Week31.java
@@ -5,7 +5,14 @@
 package week3.pkg1;
 
 /**
- *
+ *1. instalize git operation
+ * 2. git - global configration
+ * 3. git - create new empty file by clicking on the icon next to the name of 
+ * folder and name it .gitignore.
+ *     3.1  then go to google chrome and look for gitignore.io. search there for 
+ *          netbeans and copy it to the empty file.
+ * 4. git - commit 
+ * 5. git - remote - push (final step)
  * @author shabd
  */
 public class Week31 {
@@ -16,6 +23,7 @@ public class Week31 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("shabd ");
+        System.out.println("khurana");
     }
     
 }
